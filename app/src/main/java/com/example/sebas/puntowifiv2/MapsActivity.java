99ace6e9d8eac8fce924d148e5a380ce6199a1c6
Sebastian <sebastianvallejoroldan@gmail.com>
2018-11-18 18:18:26 -0500

    creación de Activities

---
+++ app/src/main/java/com/example/sebas/puntowifiv2/MapsActivity.java
@@ -0,0 +1,13 @@
+package com.example.sebas.puntowifiv2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MapsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+    }
+}
