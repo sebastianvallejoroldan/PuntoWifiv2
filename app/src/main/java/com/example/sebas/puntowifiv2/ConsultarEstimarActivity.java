f5cd5b18318a534b81940fec168ef004ee289582
Sebastian <sebastianvallejoroldan@gmail.com>
2018-11-20 08:00:09 -0500

    creando instancias de db

--- app/src/main/java/com/example/sebas/puntowifiv2/ConsultarEstimarActivity.java
+++ app/src/main/java/com/example/sebas/puntowifiv2/ConsultarEstimarActivity.java
@@ -1,7 +1,9 @@
 package com.example.sebas.puntowifiv2;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ConsultarEstimarActivity extends Activity {
 
@@ -9,5 +11,12 @@ public class ConsultarEstimarActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consultar_estimar);
+    }
+
+    void consultar (View v) {
+
+        Intent intencion = new Intent(this , EstimarActivity.class);
+
+        startActivity(intencion);
     }
 }
---
+++ app/src/main/java/com/example/sebas/puntowifiv2/DatabaseAccess.java
@@ -0,0 +1,34 @@
+package com.example.sebas.puntowifiv2;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseAccess {
+
+    private SQLiteOpenHelper openHelper;
+    private SQLiteDatabase db;
+    private static DatabaseAccess instance;
+    Cursor c= null;
+
+    private DatabaseAccess(Context context) {
+
+        this.openHelper= new DatabaseOpenHelper(context);
+    }
+
+    public static DatabaseAccess getInstance(Context context){
+
+        if(instance==null){
+
+            instance=new DatabaseAccess(context);
+        }
+        return instance;
+    }
+
+    public void open(){
+        this.db=openHelper.getWritableDatabase();
+
+    }
+
+}
